7763ac647722489f2108a88bd2ccdc5a47cd4c9e
viamouree <viamouree@icloud.com>
2026-01-11 05:11:54 +0100

    Add installation script for Node Exporter

---
+++ 03-monitoring/node_exporter/install_node_exporter.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+tar xzf node_exporter-1.7.0.linux-amd64.tar.gz
+sudo cp node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/
+
+sudo useradd -rs /bin/false node_exporter || true
+
+sudo tee /etc/systemd/system/node_exporter.service >/dev/null <<EOF
+[Unit]
+Description=Node Exporter
+
+[Service]
+User=node_exporter
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now node_exporter
